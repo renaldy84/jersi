a2471b2301d7ce3b30271eb2fe40b6f9393fd468
renaldy84 <dufronte@gmail.com>
2013-01-09 11:37:52 +0700

    create a basic caching mechanism. so we can re-use the template for further purpose. see index.html for working POC.

--- src/jersi.js
+++ src/jersi.js
@@ -4,25 +4,30 @@
 * @author Hapsoro Renaldy N <hapsoro.renaldy at gmail.com>
 * version 0.0.1
 */
-function jersi(tpl,data){
+var jersi_tpls = {};//we store all templates inside here.
+var jersi = function(tpl,data){
+
+	if(typeof jersi_tpls['_'+tpl] === 'undefined'){
+		console.log('cache template');
+		jersi_tpls['_'+tpl] = $(tpl).clone();
+	}	
+	$(tpl).html('');//reset the content
+	console.log($(tpl).html());
 	if(!$.isArray(data)){
+		$(tpl).append(jersi_tpls['_'+tpl].html());
 		$.each(data,function(name,val){
 			$(tpl+' .'+name).html(val);
 		});
 	}else{
-		var cl = $(tpl).clone();
+		console.log('multi results');
+		var cl = jersi_tpls['_'+tpl];
 		for(var i=0;i<data.length;i++){
 			d = data[i];
 			$.each(d,function(name,val){
-				if(i>0){
-					cl.find('.'+name).html(val);
-				}else{
-					$(tpl+' .'+name).html(val);
-				}
+				cl.find('.'+name).html(val);
 			});
-			if(i>0){
-				$(tpl).append(cl.html());
-			}
+			$(tpl).append(cl.html());
 		}
+		console.log($(tpl).html());
 	}
 }
